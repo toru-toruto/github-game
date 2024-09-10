d0555ff50abb645b8f74884f109422dde8439ad8
toru-toruto <toruto.torutoru.toruto@gmail.com>
2024-09-10 16:08:00 +0000

    chore: remove commented lines

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,19 +10,6 @@ export default function Home() {
   const players = useMemo(() => Array.from({ length: playerNum }), [playerNum]);
 
   return (
-    // <div className="h-screen bg-white flex flex-col">
-    //   <div className="w-full h-4/5 bg-green-100"></div>
-    //   <div className="w-full h-1/5 bg-green-400 grid grid-cols-4">
-    //     {/* <div className="flex justify-center">
-    //       <Player />
-    //     </div> */}
-    //     {Array.from({ length: playerNum }).map((_, i) => (
-    //       <div key={i} className="flex justify-center">
-    //         <Player />
-    //       </div>
-    //     ))}
-    //   </div>
-    // </div>
     <div className="h-screen bg-white flex">
       <div className="w-auto h-full bg-green-400 grid grid-rows-4 gap-4 py-4 pl-4">
         {players.map((_, i) => (
